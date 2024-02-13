6b58bf48775eb7b97d5c3e0e9a917cf88e3c62ab
Teodoro Obando <teo.obando09@gmail.com>
2024-02-12 23:55:58 -0600

    [ADD] workplace municipality jobposition and academic level foreign keys updated

--- src/people/people.service.ts
+++ src/people/people.service.ts
@@ -86,7 +86,7 @@ export class PeopleService {
       workplace, 
       municipality, 
       jobposition, 
-      identityType, 
+      identityType, // => PENDING TO UPDATE
       academicLevel, 
       ...dataToUpdate 
     } = updatePersonDto;
@@ -95,13 +95,36 @@ export class PeopleService {
       const people = await this.PersonRepository.preload({ id, ...dataToUpdate });
       if(!people) throw new NotFoundException(`La persona no ha sido encontrada`);
 
+      //Check if there is an foreignKey update and doing it if there is one
       let careerUpdated: Object;
       if(career) {
         careerUpdated = await this.CareerRepository.findOneBy({ id: career });
       }
+      let workplaceUpdated: Object;
+      if(workplace) {
+        workplaceUpdated = await this.WorkplaceRepository.findOneBy({ id: workplace });
+      }
+      let municipalityUpdated: Object;
+      if(municipality) {
+        municipalityUpdated = await this.MunicipalityRepository.findOneBy({ id: municipality });
+      }
+      let jobpositionUpdated: Object;
+      if(jobposition) {
+        jobpositionUpdated = await this.JobpositionRepository.findOneBy({ id: jobposition });
+      }
+      let academicLevelUpdated: Object;
+      if(academicLevel) {
+        academicLevelUpdated = await this.AcademicLevelRepository.findOneBy({ id: academicLevel });
+      }
 
-      return await this.PersonRepository.save({ ...people, career: careerUpdated });
-      
+      return await this.PersonRepository.save({ 
+        ...people,
+        career: careerUpdated,
+        workplace: workplaceUpdated,
+        municipality: municipalityUpdated,
+        jobposition: jobpositionUpdated,
+        academicLevel: academicLevelUpdated,
+      });
     } catch(error) { console.log(error) }
   }
 
